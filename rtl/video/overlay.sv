92a91edcb273f623c2a9482afce18a8e036df5df
Jbamidi <jassubamidi24@gmail.com>
2026-02-05 19:22:10 -0600

    Processing and Overlay Completed

--- rtl/video/overlay.sv
+++ rtl/video/overlay.sv
@@ -1,30 +1,50 @@
-module overlay(input logic [9:0] x, 
-               input logic [9:0] y, 
-               input logic vde,
-               input logic [7:0] base_red,
-               input logic [7:0] base_green,
-               input logic [7:0] base_blue,
-               output logic [7:0] red, 
-               output logic [7:0] green, 
-               output logic [7:0] blue
-               );
-    
+module overlay(
+    input logic [9:0] x, 
+    input logic [9:0] y, 
+    input logic vde,
+    input logic [7:0] base_red,
+    input logic [7:0] base_green,
+    input logic [7:0] base_blue,
+    input logic [4:0] peak_u,
+    input logic [4:0] peak_v,
+    output logic [7:0] red, 
+    output logic [7:0] green, 
+    output logic [7:0] blue
+);
+
+    logic [9:0] peak_x_start;
+    logic [9:0] peak_y_start;
+    logic [9:0] peak_x_end;
+    logic [9:0] peak_y_end;
+    logic is_peak_region;
+
     always_comb begin
+        peak_x_start = peak_u * 10'd20;
+        peak_y_start = peak_v * 10'd15;
+        peak_x_end = peak_x_start + 10'd20;
+        peak_y_end = peak_y_start + 10'd15;
+
+        if ((x >= peak_x_start) && (x < peak_x_end) && 
+            (y >= peak_y_start) && (y < peak_y_end)) begin
+            is_peak_region = 1'b1;
+        end else begin
+            is_peak_region = 1'b0;
+        end
+
         red = base_red;
-        blue = base_blue;
         green = base_green;
-        if(!vde) begin
+        blue = base_blue;
+
+        if (!vde) begin
             red = 8'h00;
             green = 8'h00;
             blue = 8'h00;
         end
-        else if ((x < 120) && (y < 60)) begin
-            red = 8'h00;
-            green = 8'hFF;
-            blue = 8'h00;
+        else if (is_peak_region) begin
+            red = 8'hFF;
+            green = 8'h00;
+            blue = 8'hFF;
         end
     end
-
-
 
 endmodule
--- rtl/video/top.sv
+++ rtl/video/top.sv
@@ -6,65 +6,121 @@ module top(input logic clk,
            output logic [2:0] hdmi_tx_n, 
            output logic [2:0] hdmi_tx_p);
         
-        logic pattern_sel;
-        assign pattern_sel = sw[0];
+      logic pattern_sel;
+      assign pattern_sel = sw[0];
 
-        logic reset;
-        assign reset = btn[0];
+      logic reset;
+      assign reset = btn[0];
 
-        logic pxl_clk;
-        logic pxl_clkx5;
-        logic locked;
-        logic hsync;
-        logic vsync;
-        logic vde;
-        logic [9:0] x;
-        logic [9:0] y;
-        logic [7:0] base_red;
-        logic [7:0] base_green;
-        logic [7:0] base_blue;
-        logic [7:0] red;
-        logic [7:0] green;
-        logic [7:0] blue;
+      logic pxl_clk;
+      logic pxl_clkx5;
+      logic locked;
+      logic hsync;
+      logic vsync;
+      logic vde;
+      logic [9:0] x;
+      logic [9:0] y;
+      
+      logic start_of_frame; 
+      logic end_of_frame;   
+      logic end_of_line;
 
+      logic [7:0] base_red;
+      logic [7:0] base_green;
+      logic [7:0] base_blue;
+      logic [7:0] red;
+      logic [7:0] green;
+      logic [7:0] blue;
 
-        clk_wiz_0 clock_wizard(.clk_in1(clk), 
-                               .reset(reset), 
-                               .pxl_clk(pxl_clk), 
-                               .pxl_clkx5(pxl_clkx5), 
-                               .locked(locked)
-                               );
-        
-        video_timing VGA(.reset(reset),
-                         .clk(pxl_clk), 
-                         .hsync(hsync), 
-                         .vsync(vsync), 
-                         .vde(vde), 
-                         .x(x), 
-                         .y(y)
-                         );
+      logic tensor_we; 
+      logic [9:0] tensor_waddr;
+      logic signed [7:0] tensor_wdata; 
+      logic tensor_valid; 
+      logic [9:0] tensor_raddr; 
+      logic signed [7:0] tensor_rdata; 
+      logic [4:0] peak_u, peak_v; 
+      logic peak_ready;
 
-        test_patterns test(.x(x), 
-                           .y(y), 
-                           .vde(vde), 
-                           .pattern_sel(pattern_sel), 
-                           .red(base_red), 
-                           .green(base_green), 
-                           .blue(base_blue)
-                           );
-        
-        overlay overlay_layer(.x(x), 
-                        .y(y), 
+      clk_wiz_0 clock_wizard(.clk_in1(clk), 
+                              .reset(reset), 
+                              .pxl_clk(pxl_clk), 
+                              .pxl_clkx5(pxl_clkx5), 
+                              .locked(locked)
+                              );
+      
+      video_timing VGA(.reset(reset),
+                        .clk(pxl_clk), 
+                        .hsync(hsync), 
+                        .vsync(vsync), 
                         .vde(vde), 
-                        .base_red(base_red), 
-                        .base_green(base_green), 
-                        .base_blue(base_blue), 
-                        .red(red), 
-                        .green(green), 
-                        .blue(blue)
+                        .x(x), 
+                        .y(y),
+                        .start_of_frame(start_of_frame), 
+                        .end_of_frame(end_of_frame), 
+                        .end_of_line(end_of_line)        
                         );
 
-        hdmi_tx_0 VGA_to_HDMI(.pix_clk(pxl_clk), 
+      test_patterns test(.x(x), 
+                        .y(y), 
+                        .vde(vde), 
+                        .pattern_sel(pattern_sel), 
+                        .red(base_red), 
+                        .green(base_green), 
+                        .blue(base_blue)
+                        );
+      
+      preprocess pre_inst(
+        .clk(pxl_clk),
+        .reset(reset),
+        .x(x),
+        .y(y),
+        .vde(vde),
+        .red(base_red),
+        .green(base_green),
+        .blue(base_blue),
+        .end_of_frame(end_of_frame),
+        .end_of_line(end_of_line),
+        .tensor_we(tensor_we),
+        .tensor_addr(tensor_waddr), 
+        .tensor_data(tensor_wdata),
+        .tensor_valid(tensor_valid)
+    );
+
+      tensor_ram ram_inst(
+         .clk(pxl_clk),
+         .we(tensor_we),
+         .waddr(tensor_waddr),
+         .wdata(tensor_wdata),
+         .raddr(tensor_raddr),
+         .rdata(tensor_rdata)
+      );
+
+      peak_detector peak_inst(
+         .clk(pxl_clk),
+         .reset(reset),
+         .tensor_valid(tensor_valid),
+         .rdata(tensor_rdata),
+         .raddr(tensor_raddr),
+         .peak_u(peak_u),
+         .peak_v(peak_v),
+         .peak_ready(peak_ready)
+      );
+
+      overlay overlay_layer(
+         .x(x),
+         .y(y),
+         .vde(vde),
+         .base_red(base_red),
+         .base_green(base_green),
+         .base_blue(base_blue),
+         .peak_u(peak_u),
+         .peak_v(peak_v),
+         .red(red),
+         .green(green),
+         .blue(blue)
+      );
+
+      hdmi_tx_0 VGA_to_HDMI(.pix_clk(pxl_clk), 
                               .pix_clkx5(pxl_clkx5), 
                               .pix_clk_locked(locked), 
                               .rst(reset), 
@@ -83,5 +139,4 @@ module top(input logic clk,
                               .TMDS_DATA_P(hdmi_tx_p), 
                               .TMDS_DATA_N(hdmi_tx_n)
                               );
-
 endmodule
